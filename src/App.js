215ffa61c0e1e9155c7eccf03b1b215e3bb1c163
MikeyF53 <mikefang530@gmail.com>
2019-07-22 18:16:47 -0400

    trying to render localstorage data

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // customerData: [],
       formData: {
         name: '',
         phone: '',
@@ -17,6 +18,7 @@ class App extends Component {
       }
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(e) {
     e.preventDefault();
@@ -28,12 +30,31 @@ class App extends Component {
       }
     }));
   }
+  // When submitted it stores in state in the INFO object
+  handleSubmit(e) {
+    e.preventDefault()
+    const customer = {
+      name: this.state.formData.name,
+      phone: this.state.formData.phone,
+      adults: this.state.formData.adults,
+      highChair: this.state.formData.highChair,
+      booster: this.state.formData.booster,
+      message: this.state.formData.message,
+    }
+    const oldItems = JSON.parse(localStorage.getItem('customer')) || [];
+    oldItems.push(customer);
+    localStorage.setItem('customer',JSON.stringify(oldItems));// storing state in LS
+    // console.log(oldItems.forEach(e => {console.log(e.name)}));
+    
+  }
+
   render() {
     return (
       <div>
         <RsvpForm
           formData={this.state.formData}
           handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
         />
         <WaitingList
           formData={this.state.formData}
--- src/components/WaitingList.jsx
+++ src/components/WaitingList.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import RsvpForm from './RsvpForm'
+import RsvpForm from './RsvpForm';
 
 const WaitingList = props => {
-console.log(props);
-
+  // console.log(props);
+  const customerInfo = JSON.parse(localStorage.getItem('customer'));
   return (
     <div>
       <h1>Waiting List</h1>
-      <p>{props.formData.name}</p>
+      <div>
+        {customerInfo.name}
+      </div>
     </div>
   );
-  
 };
 
 export default WaitingList;
